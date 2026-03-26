c99c888e254728b452b72d80933b0175a67fc760
kombifxx <arinaghost@gmail.com>
2026-03-26 21:23:13 +0300

    del crud /testing

--- src/routes/testing.ts
+++ src/routes/testing.ts
@@ -3,7 +3,7 @@ import { db } from '../db/in-memory.db'
 export const testingRouter = Router()
 
 testingRouter
- .delete('/testing/all-data', (_req, res: Response) => {
+ .delete('/all-data', (_req, res: Response) => {
     db.videos = []
     res.sendStatus(204)
 })
